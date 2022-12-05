36f1fec505e8e1f29e5fb5358d01617b2aa0d384
kaislazreg <lazreg.kaiss@gmail.com>
2022-12-05 12:10:53 +0100

    express1

---
+++ middleware/data.js
@@ -0,0 +1,17 @@
+const date=(req,res,next)=>{
+    let date = new Date();
+    let h = date.getHours();
+    let d =date.getDay();
+
+
+    if( !(h <= 9 && h >= 17) || ( d == 0 || d == 6 ) )  {
+        res.send('close') 
+    }
+    else{
+        res.sendFile(__dirname +'/horaire.html' )
+    }
+    next()
+
+}
+
+module.exports=date
